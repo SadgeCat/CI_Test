6ac9aa33682def3927097c3b91f90a57a376a06a
Yuhang Pan <YuchenPan@MacBookAir.fios-router.home>
2026-02-02 20:59:04 -0500

    fix error

--- app.py
+++ app.py
@@ -9,21 +9,21 @@ import urllib.parse
 
 app = Flask(__name__) 
 
-with open("key_nasa.txt") as file:
-    nasa_key = file.read()
-
-with urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=" + nasa_key) as response:
-    page = response.read()
-    # page is in bites, not a dict
-
-dict = json.loads(page.decode('utf-8'))
-# use json to convert page from bytes to a usable dict
-desc = dict['explanation']
-# pulls explanation 
-imgurl = dict["hdurl"]
-
 @app.route('/')
 def gaming():
+    with open("key_nasa.txt") as file:
+        nasa_key = file.read()
+
+    with urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=" + nasa_key) as response:
+        page = response.read()
+        # page is in bites, not a dict
+
+    dict = json.loads(page.decode('utf-8'))
+    # use json to convert page from bytes to a usable dict
+    desc = dict['explanation']
+    # pulls explanation 
+    imgurl = dict["hdurl"]
+    
     print(dict)
     print("/n") 
     print(desc)
