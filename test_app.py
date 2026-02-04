25f603c1b45ea02a6b0f1a4b9a4f46b91237e3eb
Yuhang Pan <YuchenPan@MacBookAir.fios-router.home>
2026-02-03 19:50:44 -0500

    test wrong route error

--- test_app.py
+++ test_app.py
@@ -1,10 +1,10 @@
-# # Yuhang Pan
-# # SoftDev 2026
-# # Continuous Integration DEMO
+# Yuhang Pan
+# SoftDev 2026
+# Continuous Integration DEMO
 
-# from app import app
+from app import app
 
-# def test_page():
-#     client = app.test_client()
-#     response = client.get('/')
-#     assert response.status_code == 200
+def test_page():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
