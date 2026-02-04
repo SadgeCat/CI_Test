b91e5590ab25d7ec5d70828feea6055731f5401a
Yuhang Pan <YuchenPan@MacBookAir.fios-router.home>
2026-02-03 20:18:44 -0500

    update workflow with coments

--- test_app.py
+++ test_app.py
@@ -6,6 +6,7 @@ from app import app
 
 # has `test` in the func name as well as file name for pytest to run
 def test_page():
-    client = app.test_client()      # creates a browser without a live server to test your app
-    response = client.get('/home')      # this client can do get/post requests which returns a response which usually contains the status code and data
+    client = app.test_client()      # emulates a browser without a live server to test your app
+    response = client.get('/')      # this client can do get/post requests which returns a response which usually contains the status code and data
+    # this assert keyword asserts that the statement is true, so if the statement turns out to be false, that means something went wrong, this is often used for testing
     assert response.status_code == 200      # status code 200 is a successful request so this checks if our apps, the routes work without errors
