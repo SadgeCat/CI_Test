09bb11154c2b1a596e42491ff60b6e29cf5b6589
Yuhang Pan <YuchenPan@MacBookAir.fios-router.home>
2026-02-03 20:02:48 -0500

    add comments & change route

--- test_app.py
+++ test_app.py
@@ -4,7 +4,8 @@
 
 from app import app
 
+# has `test` in the func name as well as file name for pytest to run
 def test_page():
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
+    client = app.test_client()      # creates a browser without a live server to test your app
+    response = client.get('/home')      # this client can do get/post requests which returns a response which usually contains the status code and data
+    assert response.status_code == 200      # status code 200 is a successful request so this checks if our apps, the routes work without errors
